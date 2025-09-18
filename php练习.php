3628e2cf8d3701b32d963234561c2ca8b185e85f
wengyucheng <wengyucheng30@gmail.com>
2025-09-18 17:27:31 +0900

    style.cssで作成した内容

---
+++ php练习.php
@@ -0,0 +1,18 @@
+<?php
+echo "Guten Tag!"
+echo <br>
+echo "Ich kann das Deutsch sprechen."
+
+$language = "Deutsch (German)"
+
+echo $language
+?>
+
+<?php
+$a = 18
+
+if ($a >= 18) {
+    echo "成年人"
+} else {
+    echo "未成年人"
+}
